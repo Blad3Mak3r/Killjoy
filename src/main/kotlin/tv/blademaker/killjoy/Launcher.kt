123d939c9dfda1503b7af7f36d50ae180103de2c
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-13 16:29:10 +0200

    Update Launcher.kt

--- src/main/kotlin/tv/blademaker/killjoy/Launcher.kt
+++ src/main/kotlin/tv/blademaker/killjoy/Launcher.kt
@@ -1,15 +1,25 @@
 package tv.blademaker.killjoy
 
+import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.entities.ApplicationInfo
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
 import net.dv8tion.jda.api.sharding.ShardManager
+import net.dv8tion.jda.api.utils.Compression
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 import net.hugebot.ratelimiter.RateLimiter
-import org.json.JSONArray
-import org.json.JSONObject
 import org.slf4j.LoggerFactory
-import tv.blademaker.killjoy.core.Agent
+import tv.blademaker.killjoy.framework.CommandRegistry
+import tv.blademaker.killjoy.utils.Loaders
+import tv.blademaker.killjoy.valorant.Agent
+import java.rmi.registry.LocateRegistry
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import kotlin.system.exitProcess
 
 object Launcher {
+
+    val scheduler = Executors.newSingleThreadScheduledExecutor()
 
     lateinit var shardManager: ShardManager
         private set
@@ -20,18 +30,58 @@ object Launcher {
     lateinit var info: ApplicationInfo
         private set
 
-    lateinit var agents: List<Agent>
+    lateinit var commandRegistry: CommandRegistry
         private set
+
+    private lateinit var agents: Set<Agent>
 
     val rateLimiter: RateLimiter = RateLimiter.Builder().setQuota(20).setExpirationTime(1, TimeUnit.MINUTES).build()
 
     @JvmStatic
     fun main(args: Array<String>) {
 
-        agents = Loaders.loadAgents()
+        try {
+            agents = Loaders.loadAgents()
+        } catch (e: Throwable) {
+            exitProcess(0)
+        }
 
+        commandRegistry = CommandRegistry()
+
+        shardManager = DefaultShardManagerBuilder.createLight(System.getenv("TOKEN"))
+            .setActivity(Activity.playing("Valorant | joy help"))
+            .setEnableShutdownHook(false)
+            .addEventListeners(commandRegistry)
+            .setCompression(Compression.ZLIB)
+            .enableIntents(
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.DIRECT_MESSAGES,
+                GatewayIntent.GUILD_MEMBERS,
+            )
+            .disableIntents(
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_PRESENCES
+            )
+            .enableCache(CacheFlag.MEMBER_OVERRIDES)
+            .disableCache(
+                CacheFlag.VOICE_STATE,
+                CacheFlag.ACTIVITY,
+                CacheFlag.EMOTE
+            )
+            .build()
 
     }
+
+    fun getAgent(name: String) = agents.find { it.name.equals(name, true) }
+
+    fun getAgentsByRole(name: String): List<Agent>? {
+        val result = kotlin.runCatching { Agent.Role.of(name) }
+        if (result.isFailure) return null
+        return agents.filter { it.role === result.getOrNull()!! }
+    }
+
+    fun getAgents() = agents
 
     private val log = LoggerFactory.getLogger(Launcher::class.java)
 }
