824e865e4d281fb31dcde5f2991c80b900777f1f
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-13 22:04:37 +0200

    Update AgentCommand.kt

--- src/main/kotlin/tv/blademaker/killjoy/commands/AgentCommand.kt
+++ src/main/kotlin/tv/blademaker/killjoy/commands/AgentCommand.kt
@@ -2,13 +2,14 @@ package tv.blademaker.killjoy.commands
 
 import tv.blademaker.killjoy.Launcher
 import tv.blademaker.killjoy.framework.Category
+import tv.blademaker.killjoy.framework.ColorExtra
 import tv.blademaker.killjoy.framework.CommandArgument
 import tv.blademaker.killjoy.framework.CommandContext
 import tv.blademaker.killjoy.framework.abs.Command
 import tv.blademaker.killjoy.framework.annotations.CommandMeta
 import tv.blademaker.killjoy.utils.Emojis
 
-@CommandMeta("agents", Category.Information, aliases = ["agent"])
+@CommandMeta("agents", Category.Basic, aliases = ["agent"])
 class AgentCommand : Command() {
     override fun handle(ctx: CommandContext) {
 
@@ -23,15 +24,32 @@ class AgentCommand : Command() {
                 setFooter("If you want to get more information about an agent use \"joy agents agent_name\"")
             }.queue()
 
-        } else {
+        } else if (ctx.args.size == 1) {
             val agent = Launcher.getAgent(ctx.args[0]) ?: return ctx.send(Emojis.NoEntry, "That agent does not exists...").queue()
 
             ctx.send(agent.asEmbed().build()).queue()
+        } else {
+            val agent = Launcher.getAgent(ctx.args[0]) ?: return ctx.send(Emojis.NoEntry, "That agent does not exists...").queue()
+
+            val skill = agent.skills.find { it.button.name.equals(ctx.args[1], true) }
+                ?: return ctx.send(Emojis.NoEntry, "I have not been able to find that skill...").queue()
+
+            ctx.embed {
+                setAuthor(agent.name, null, agent.avatar)
+                setTitle(skill.name)
+                setDescription(skill.info)
+                setThumbnail(skill.iconUrl)
+                setImage(skill.preview)
+                addField("Action Button", skill.button.name, true)
+                addField("Usage Cost", skill.cost, true)
+                setColor(ColorExtra.VAL_RED)
+            }.queue()
         }
     }
 
     override val args: List<CommandArgument>
         get() = listOf(
-            CommandArgument("agent_name", "An agent name [jett]", false)
+            CommandArgument("agent_name", "An agent name [jett]", false),
+            CommandArgument("skill_button", "The button used to use this skill [q]", false)
         )
 }
