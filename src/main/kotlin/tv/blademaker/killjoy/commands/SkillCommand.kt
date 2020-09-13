e7f90a0dff6b6a91031ba42bc79f70b62c30124e
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-13 19:46:38 +0200

    Create SkillCommand.kt

---
+++ src/main/kotlin/tv/blademaker/killjoy/commands/SkillCommand.kt
@@ -0,0 +1,39 @@
+package tv.blademaker.killjoy.commands
+
+import tv.blademaker.killjoy.Launcher
+import tv.blademaker.killjoy.framework.Category
+import tv.blademaker.killjoy.framework.ColorExtra
+import tv.blademaker.killjoy.framework.CommandArgument
+import tv.blademaker.killjoy.framework.CommandContext
+import tv.blademaker.killjoy.framework.abs.Command
+import tv.blademaker.killjoy.framework.annotations.CommandMeta
+import tv.blademaker.killjoy.utils.Emojis
+import tv.blademaker.killjoy.utils.Utils
+
+@CommandMeta("skills", Category.Basic, aliases = ["skill"])
+class SkillCommand : Command() {
+    override fun handle(ctx: CommandContext) {
+        if (ctx.args.isEmpty()) return Utils.Commands.replyWrongUsage(ctx, this)
+
+        val skill = Launcher.getSkills().find { it.id.equals(ctx.args[0], true) }
+            ?: return ctx.send(Emojis.NoEntry, "I have not been able to find that skill...").queue()
+
+        val agent = Launcher.getAgents().find { it.skills.any { s -> s === skill } }!!
+
+        ctx.embed {
+            setAuthor(agent.name, null, agent.avatar)
+            setTitle(skill.name)
+            setDescription(skill.info)
+            setThumbnail(skill.iconUrl)
+            setImage(skill.preview)
+            addField("Action Button", skill.button.name, true)
+            addField("Usage Cost", skill.cost, true)
+            setColor(ColorExtra.VAL_RED)
+        }.queue()
+    }
+
+    override val args: List<CommandArgument>
+        get() = listOf(
+            CommandArgument("skill_name", "A Skill name [shock-bolt]", true)
+        )
+}
