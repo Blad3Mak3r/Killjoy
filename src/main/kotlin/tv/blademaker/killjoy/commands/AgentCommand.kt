08a62fd01d4da17c81ad80848bbf50bac609bc89
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-13 16:31:30 +0200

    Create AgentCommand.kt

---
+++ src/main/kotlin/tv/blademaker/killjoy/commands/AgentCommand.kt
@@ -0,0 +1,31 @@
+package tv.blademaker.killjoy.commands
+
+import tv.blademaker.killjoy.Launcher
+import tv.blademaker.killjoy.framework.Category
+import tv.blademaker.killjoy.framework.CommandContext
+import tv.blademaker.killjoy.framework.abs.Command
+import tv.blademaker.killjoy.framework.annotations.CommandMeta
+import tv.blademaker.killjoy.utils.Emojis
+
+@CommandMeta("agents", Category.Information)
+class AgentCommand : Command() {
+    override fun handle(ctx: CommandContext) {
+
+        if (ctx.args.isEmpty()) {
+            val agents = Launcher.getAgents()
+
+            ctx.embed {
+                setTitle("Valorant Agents")
+                for (agent in agents) {
+                    addField("${agent.role.emoji} - ${agent.name}", agent.bio, true)
+                }
+                setFooter("If you want to get more information about an agent use \"joy agents agent_name\"")
+            }.queue()
+
+        } else {
+            val agent = Launcher.getAgent(ctx.args[0]) ?: return ctx.send(Emojis.NoEntry, "That agent does not exists...").queue()
+
+            ctx.send(agent.asEmbed().build()).queue()
+        }
+    }
+}
