10eb94603fdf83ac8a9e9e0b30f3ca80acf944a1
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-14 12:07:01 +0200

    Added loadArsenal() to Loaders

--- src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
+++ src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
@@ -3,13 +3,14 @@ package tv.blademaker.killjoy.utils
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import tv.blademaker.killjoy.valorant.Agent
+import tv.blademaker.killjoy.valorant.Weapon
 
 object Loaders {
 
     private val log = LoggerFactory.getLogger(Loaders::class.java)
 
-    fun loadAgents(): Set<Agent> {
-        val list = mutableSetOf<Agent>()
+    fun loadAgents(): List<Agent> {
+        val list = mutableListOf<Agent>()
 
         val agentsIndex = this::class.java.getResource("/agents/agents.txt").readText().split("\n")
 
@@ -33,5 +34,26 @@ object Loaders {
 
     private fun loadMaps() {}
 
-    private fun loadWeapons() {}
+    fun loadArsenal(): List<Weapon> {
+        val list = mutableListOf<Weapon>()
+
+        val arsenalIndex = this::class.java.getResource("/arsenal/arsenal.txt").readText().split("\n")
+
+        if (arsenalIndex.isEmpty()) throw IllegalStateException("Arsenal Index cannot be empty or null")
+
+        for (weaponName in arsenalIndex) {
+            val file = this::class.java.getResource("/arsenal/${weaponName.toLowerCase()}.json")
+                    ?: throw IllegalStateException("$weaponName.json is not present")
+
+            val fileContent = file.readText()
+
+            if (fileContent.isEmpty()) throw IllegalStateException("$weaponName.json is empty")
+
+            list.add(Weapon(JSONObject(fileContent)))
+        }
+
+        log.info("Loaded ${list.size} weapons from arsenal!! [${list.joinToString(", ") { it.name }}]")
+
+        return list
+    }
 }
