75e1fcd9acf9b58122129965d322be0d9bd666ed
Blad3Mak3r <blademaker.live@gmail.com>
2020-09-15 17:25:49 +0200

    Update Loaders.kt

--- src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
+++ src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
@@ -11,49 +11,35 @@ object Loaders {
 
     fun loadAgents(): List<Agent> {
         val list = mutableListOf<Agent>()
-
         val agentsIndex = this::class.java.getResource("/agents/agents.txt").readText().split("\n")
-
         if (agentsIndex.isEmpty()) throw IllegalStateException("Agents Index cannot be empty or null")
 
         for (agentName in agentsIndex) {
             val file = this::class.java.getResource("/agents/${agentName.toLowerCase()}.json")
                 ?: throw IllegalStateException("$agentName.json is not present")
-
             val fileContent = file.readText()
-
             if (fileContent.isEmpty()) throw IllegalStateException("$agentName.json is empty")
-
             list.add(Agent(JSONObject(fileContent)))
         }
 
         log.info("Loaded ${list.size} agents!! [${list.joinToString(", ") { it.name }}]")
-
         return list
     }
 
-    private fun loadMaps() {}
-
     fun loadArsenal(): List<Weapon> {
         val list = mutableListOf<Weapon>()
-
         val arsenalIndex = this::class.java.getResource("/arsenal/arsenal.txt").readText().split("\n")
-
         if (arsenalIndex.isEmpty()) throw IllegalStateException("Arsenal Index cannot be empty or null")
 
         for (weaponName in arsenalIndex) {
             val file = this::class.java.getResource("/arsenal/${weaponName.toLowerCase()}.json")
                     ?: throw IllegalStateException("$weaponName.json is not present")
-
             val fileContent = file.readText()
-
             if (fileContent.isEmpty()) throw IllegalStateException("$weaponName.json is empty")
-
             list.add(Weapon(JSONObject(fileContent)))
         }
 
         log.info("Loaded ${list.size} weapons from arsenal!! [${list.joinToString(", ") { it.name }}]")
-
         return list
     }
 }
