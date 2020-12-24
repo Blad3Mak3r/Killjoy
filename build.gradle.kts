5345906aa93deff625e5c4db00d78e6a01662abc
Blad3Mak3r <blademaker.live@gmail.com>
2020-12-24 08:09:17 +0100

    Update gradle and fix ShadowJar classpath.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-plugins {
-    kotlin("jvm") version "1.4.20"
+val ktorVersion = "1.5.0"
+val coroutinesVersion = "1.4.2-native-mt"
 
+plugins {
+    kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "6.1.0"
+
     application
+    java
     idea
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
@@ -19,16 +28,13 @@ version = "0.3.4"
 repositories {
     mavenCentral()
     jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlinx")
     maven("https://jitpack.io")
 }
-
-val ktorVersion = "1.4.2"
-val coroutinesVersion = "1.4.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
 
     //  HugeBot dependencies
     implementation("net.hugebot:RateLimiter:v1.0.0")
@@ -40,7 +46,7 @@ dependencies {
     //  Logging
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    implementation("io.sentry:sentry:1.7.30")
+    implementation("io.sentry:sentry:3.2.0")
     implementation("com.google.guava:guava:30.0-jre")
 
     //  Ktor HTTP Client
