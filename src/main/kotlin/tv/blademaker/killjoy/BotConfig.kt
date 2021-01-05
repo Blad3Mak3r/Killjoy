e241791008e2553fc370d485476f1c1e56d1430e
Blad3Mak3r <blademaker.live@gmail.com>
2021-01-05 23:37:26 +0100

    Update Copyright

--- src/main/kotlin/tv/blademaker/killjoy/BotConfig.kt
+++ src/main/kotlin/tv/blademaker/killjoy/BotConfig.kt
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020. Blademaker
+ * Copyright (c) 2021. Blademaker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 
 package tv.blademaker.killjoy
 
-import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import java.io.File
 import java.lang.IllegalStateException
