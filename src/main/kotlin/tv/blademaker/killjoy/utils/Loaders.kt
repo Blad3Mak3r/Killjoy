7b674eb03290bb1f9d2509ab2663a94ec8f1c932
Blad3Mak3r <blademaker.live@gmail.com>
2021-01-26 09:03:23 +0100

    Create TOP command

--- src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
+++ src/main/kotlin/tv/blademaker/killjoy/utils/Loaders.kt
@@ -15,8 +15,10 @@
 
 package tv.blademaker.killjoy.utils
 
+import org.json.JSONArray
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
+import tv.blademaker.killjoy.apis.riot.entities.RankedPlayer
 import tv.blademaker.killjoy.valorant.ValorantAgent
 import tv.blademaker.killjoy.valorant.ValorantEntity
 import tv.blademaker.killjoy.valorant.ValorantMap
@@ -35,6 +37,43 @@ object Loaders {
     fun loadAgents(): List<ValorantAgent> = loadValorantEntities(ValorantAgent::class.java, "agents")
     fun loadArsenal(): List<ValorantWeapon> = loadValorantEntities(ValorantWeapon::class.java, "arsenal")
     fun loadMaps(): List<ValorantMap> = loadValorantEntities(ValorantMap::class.java, "maps")
+
+    fun loadLeaderboards(): Map<String, List<RankedPlayer>> {
+        val map = mutableMapOf<String, List<RankedPlayer>>()
+
+        val path = "leaderboards"
+        val index = getResourceFiles(path)
+
+        check(index.isNotEmpty()) { "LEADERBOARDS index cannot be empty or null." }
+
+        for (fileName in index) {
+            val file = this::class.java.getResource("/$path/$fileName")
+                ?: throw IllegalStateException("/$path/$fileName is not present")
+
+            val fileContent = file.readText()
+            if (fileContent.isEmpty()) throw IllegalStateException("$path/$fileName is empty")
+
+            val region = fileName.toUpperCase().removeSuffix(".JSON")
+
+            val players = JSONArray(fileContent).map {
+                it as JSONObject
+                RankedPlayer(
+                    it.getString("puuid"),
+                    it.getString("gameName"),
+                    it.getString("tagLine"),
+                    it.getInt("leaderboardRank"),
+                    it.getInt("rankedRating"),
+                    it.getInt("numberOfWins")
+                )
+            }.sortedBy { it.leaderboardRank }
+
+            map[region] = players
+        }
+
+        log.info("Loaded ${map.size} region leaderboards!! [${map.keys.joinToString(", ")}]")
+
+        return map
+    }
 
     /**
      * Load a list of provided [ValorantEntity] based class.
