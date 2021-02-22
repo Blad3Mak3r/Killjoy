e5c8e21dcc4b619bbe54c1ebcd0a076050274e77
Blad3Mak3r <blademaker.live@gmail.com>
2021-02-22 17:24:45 +0100

    Include Useful links to basic help command

--- src/main/kotlin/tv/blademaker/killjoy/commands/info/HelpCommand.kt
+++ src/main/kotlin/tv/blademaker/killjoy/commands/info/HelpCommand.kt
@@ -15,7 +15,10 @@
 
 package tv.blademaker.killjoy.commands.info
 
+import tv.blademaker.killjoy.INVITE_URL
 import tv.blademaker.killjoy.Launcher
+import tv.blademaker.killjoy.REPOSITORY_URL
+import tv.blademaker.killjoy.WEBSITE_URL
 import tv.blademaker.killjoy.framework.Category
 import tv.blademaker.killjoy.framework.ColorExtra
 import tv.blademaker.killjoy.framework.annotations.CommandArgument
@@ -50,6 +53,8 @@ class HelpCommand : Command() {
                             Launcher.commandRegistry.getCommands(category).joinToString(" ") { "``${it.props.name}``" },
                             true)
                 }
+                addBlankField(false)
+                addField("Useful links", "[Add Bot]($INVITE_URL) | [Repository]($REPOSITORY_URL) | [Website]($WEBSITE_URL)", false)
             }.queue()
         } else {
             val invoke = ctx.args[0]
