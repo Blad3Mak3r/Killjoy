0d28a48b00d38056abd8dccc823e96cd6aa90ae8
Blad3Mak3r <blademaker.live@gmail.com>
2021-02-22 17:24:08 +0100

    Create constants object

---
+++ src/main/kotlin/tv/blademaker/killjoy/Constants.kt
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2021. Blademaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ ******************************************************************************/
+
+package tv.blademaker.killjoy
+
+const val INVITE_URL = "https://discord.com/oauth2/authorize?client_id=706887214088323092&permissions=321600&scope=bot%20applications.commands"
+const val REPOSITORY_URL = "https://github.com/Blad3Mak3r/Killjoy"
+const val WEBSITE_URL = "https://killjoy.blademaker.tv"
