44699086bda2959c3bbd90869cb3b5719756e5cf
Blad3Mak3r <blademaker.live@gmail.com>
2021-06-25 01:15:58 +0200

    v0.12.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val prometheusVersion = "0.11.0"
 val sentryVersion = "5.0.1"
 
 group = "dev.killjoy"
-val versionObj = Version(0, 12, 4)
+val versionObj = Version(0, 12, 5)
 version = versionObj.build()
 
 repositories {
